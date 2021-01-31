9f8000c490afd8a332e4eb32cc45857eb61c8f0f
alecapra96 <alejandrocapraargentina@gmail.com>
2021-01-31 11:23:11 -0300

    tarea 9

--- mastarea.js
+++ mastarea.js
@@ -310,3 +310,13 @@ function addListeners(resultsElements) {
 // EliminarCaracteres("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz");
 
 // 9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
+
+function numeroRamdon(){
+    let numero =Math.random() * (600 - 501) + 501;
+    return numero.toFixed();
+
+}
+console.log(numeroRamdon());
+
+// 0) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro),
+//  pe. miFuncion(2002) devolverá true.
