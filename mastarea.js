83fffd9242ec42722673df581667cfe9d9d1edb4
alecapra96 <alejandrocapraargentina@gmail.com>
2021-01-30 21:48:56 -0300

    Update mastarea.js

--- mastarea.js
+++ mastarea.js
@@ -24,17 +24,13 @@
 // 8) Programa una función que elimine cierto patrón de caracteres de un texto dado,
 //  pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
 
-// 5) Programa una función que invierta las palabras de una cadena de texto, 
-// pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".
+// 9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
 
-// 6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, 
-// pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
+// 10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro),
+//  pe. miFuncion(2002) devolverá true.
 
-// 7) Programa una función que valide si una palabra o frase dada, es un palíndromo
-//  (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
-
-// 8) Programa una función que elimine cierto patrón de caracteres de un texto dado,
-//  pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+// 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el
+//      producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
 
 // 12) Programa una función que determine si un número es primo (aquel que solo es divisible por sí mismo y 1) o no, 
 // pe. miFuncion(7) devolverá true.
@@ -263,4 +259,14 @@ function ocultarRespuesta9(){
 // ___________________________________________________________________________________________________
 
 
+// 8) Programa una función que elimine cierto patrón de caracteres de un texto dado,
+//  pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
 
+// function EliminarCaracteres(string,comparacion){
+//     string = string.replaceAll(comparacion,"");
+//     console.log(string);
+//     return string;
+// }
+// EliminarCaracteres("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz");
+
+// 9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
